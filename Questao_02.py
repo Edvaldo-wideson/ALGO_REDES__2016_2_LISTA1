b851ce688622eed378ff28720dc05ddfa6f726ff
Edvaldo-wideson <tinho_garfield@hotmail.com>
2016-08-18 22:38:29 -0300

    Add files via upload

---
+++ Questao_02.py
@@ -0,0 +1,17 @@
+
+
+
+frase = ('Eu tenho um sonho. O sonho de ver meus filhos julgados por sua personalidade, não pela cor de sua pele.')
+novafrase = frase.upper()
+print(novafrase)
+
+frase2 = ('“Eu tenho um sonho. O sonho de ver meus filhos julgados por sua personalidade, não pela cor de sua pele.” ')
+novafrase2 = frase2.lower()
+print(novafrase2)
+
+frase3 = ('“Eu tenho um sonho. O sonho de ver meus filhos julgados por sua personalidade, não pela cor de sua pele.” ')
+novafrase3 = frase3.title()
+print(novafrase3)
+
+
+
---
+++ Questao_03.py
@@ -0,0 +1,19 @@
+
+
+print ('Programa de Resultados')
+print()
+
+soma = 200 + 250
+print('O Resultado da Soma 200 + 250 = ',soma)
+
+divisao = 200 / 8
+print('O Resultado da Divisao 200 / 8 =', divisao)
+
+multiplicaçao = 200 * 7
+print('O Resultado da Multiplicaçao 200 * 7 =', multiplicaçao)
+
+subtração = 800 - 10
+print('O Resultado da Subtracao 800 - 10 =', subtração)
+
+potencia = 2 * 10
+print('O Resultado da Potencia 2 * 10 =', potencia)
